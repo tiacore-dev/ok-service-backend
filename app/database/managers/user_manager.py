22ff1e8100b3b09c8e124601ce8185bacca2ae89
Viktoria <v.s.shuvaeva@gmail.com>
2024-12-18 12:36:37 +0700

    fix admin

--- app/database/managers/user_manager.py
+++ app/database/managers/user_manager.py
@@ -8,6 +8,24 @@ class UserManager(BaseDBManager):
     def model(self):
         return Users
 
+    def add_user(self, login, password, name, role, category=None):
+        """Добавление нового пользователя с хешированием пароля."""
+        with self.session_scope() as session:
+            new_user = self.model(
+                login=login,
+                name=name,
+                role=role,
+                category=category if category else None,
+                deleted=False
+            )
+            # Установим пароль сразу после создания объекта
+            new_user.set_password(password)
+
+            session.add(new_user)
+            # При выходе из контекстного менеджера произойдёт commit
+            return new_user
+
+
     def check_password(self, username, password):
         """Проверяем пароль пользователя"""
         with self.session_scope() as session:
