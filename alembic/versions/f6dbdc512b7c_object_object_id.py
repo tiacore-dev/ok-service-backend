ab6e2652145e8cff767a278b56fbfcb9850aaea0
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-17 13:18:09 +0700

    migration fix

--- alembic/versions/f6dbdc512b7c_object_object_id.py
+++ alembic/versions/f6dbdc512b7c_object_object_id.py
@@ -19,28 +19,54 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade():
-    # Удаляем старый первичный ключ
+    # Удаляем внешний ключ из таблицы projects
+    with op.batch_alter_table('projects') as batch_op:
+        batch_op.drop_constraint('projects_object_fkey', type_='foreignkey')
+
+    # Удаляем первичный ключ из таблицы objects
     with op.batch_alter_table('objects') as batch_op:
         batch_op.drop_constraint('objects_pkey', type_='primary')
 
-    # Переименовываем столбец
+    # Переименовываем столбец object в object_id
     op.alter_column('objects', 'object',
                     new_column_name='object_id', existing_type=sa.String())
 
-    # Назначаем новый первичный ключ
+    # Создаем новый первичный ключ
     with op.batch_alter_table('objects') as batch_op:
         batch_op.create_primary_key('objects_pkey', ['object_id'])
 
+    # Восстанавливаем внешний ключ в таблице projects
+    with op.batch_alter_table('projects') as batch_op:
+        batch_op.create_foreign_key(
+            'projects_object_fkey',  # Имя ограничения
+            'objects',              # Ссылаемая таблица
+            ['object'],             # Поле в таблице projects
+            ['object_id']           # Поле в таблице objects
+        )
+
 
 def downgrade():
+    # Удаляем внешний ключ из таблицы projects
+    with op.batch_alter_table('projects') as batch_op:
+        batch_op.drop_constraint('projects_object_fkey', type_='foreignkey')
+
     # Удаляем новый первичный ключ
     with op.batch_alter_table('objects') as batch_op:
         batch_op.drop_constraint('objects_pkey', type_='primary')
 
-    # Переименовываем столбец обратно
+    # Переименовываем столбец обратно с object_id на object
     op.alter_column('objects', 'object_id',
                     new_column_name='object', existing_type=sa.String())
 
     # Восстанавливаем старый первичный ключ
     with op.batch_alter_table('objects') as batch_op:
         batch_op.create_primary_key('objects_pkey', ['object'])
+
+    # Восстанавливаем внешний ключ в таблице projects
+    with op.batch_alter_table('projects') as batch_op:
+        batch_op.create_foreign_key(
+            'projects_object_fkey',  # Имя ограничения
+            'objects',               # Ссылаемая таблица
+            ['object'],              # Поле в таблице projects
+            ['object']               # Поле в таблице objects
+        )
