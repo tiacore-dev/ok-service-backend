77117894126de464dd5a592f7677d3ff4a300634
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-18 15:54:50 +0700

    session prod

--- app/database/db_setup.py
+++ app/database/db_setup.py
@@ -7,12 +7,13 @@ Base = declarative_base()
 
 def init_db(database_url, config_name):
     engine = create_engine(database_url, echo=False)
-    # Session = sessionmaker(bind=engine) #Для прода
-    session_factory = sessionmaker(bind=engine)  # Для тестов
-    Session = scoped_session(session_factory)  # Для тестов
+
     if config_name == "testing":
+        session_factory = sessionmaker(bind=engine)  # Для тестов
+        Session = scoped_session(session_factory)  # Для тестов
         from app.database.models import Users, ObjectStatuses, Logs, Roles  # pylint: disable=unused-import
         # Создание всех таблиц
         Base.metadata.create_all(engine)
-
+    else:
+        Session = sessionmaker(bind=engine)  # Для прода
     return engine, Session, Base
