edd714889f4dee33d78ae3eb92306eae5c69487d
Viktoria <v.s.shuvaeva@gmail.com>
2025-02-17 17:27:33 +0700

    logger

--- app/utils/db_users.py
+++ app/utils/db_users.py
@@ -1,4 +1,7 @@
+import logging
 import pandas as pd
+
+logger = logging.getLogger('ok_service')
 
 
 def put_users_in_db(admin_id):
@@ -7,14 +10,17 @@ def put_users_in_db(admin_id):
 
     # Читаем данные из Excel
     df_users = pd.read_excel(file_path)
-    print(df_users.head())  # Посмотрим первые строки
+    logger.info(df_users.head())  # Вывести первые строки
+    logger.info(len(df_users))  # Количество строк
+
     # Импортируем менеджер пользователей
     from app.database.managers.user_manager import UserManager
 
     user_manager = UserManager()
     created_by = admin_id
     # Добавляем пользователей в базу
-    for _, row in df_users.iterrows():
+    for index, row in df_users.iterrows():
+        logger.info(f"Processing row {index}: {row.to_dict()}")
         user_manager.add_user(
             login=row["Логин"],
             password=str(row["Пароль"]),
@@ -22,9 +28,10 @@ def put_users_in_db(admin_id):
             role=row["Роль"],
             created_by=created_by,
             category=int(row["Категория"]) if pd.notna(
-                row["Категория"]) else None  # Явная проверка
+                row["Категория"]) else None
         )
-        print(str(row['Пароль']))
 
-    print("Пользователи успешно добавлены в базу данных!")
+        logger.info(str(row['Пароль']))
+
+    logger.info("Пользователи успешно добавлены в базу данных!")
     # Сохраняем таблицу в Exceд
