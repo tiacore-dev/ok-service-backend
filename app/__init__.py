1034ad980015b0090b7a5ba9b25aa7dabbf9652f
Viktoria <v.s.polikanova@gmail.com>
2025-06-29 10:32:39 +0700

    master

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,34 @@
 import time
-from flask_jwt_extended import JWTManager
-from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
-from flask import Flask
-from flask import request, g
+
+from flask import Flask, g, request
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, get_jwt_identity, verify_jwt_in_request
+from flask_marshmallow import Marshmallow
 from flask_restx import Api
-from prometheus_flask_exporter import PrometheusMetrics
-from prometheus_client import REGISTRY
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry import trace
-from opentelemetry.sdk.resources import Resource
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from prometheus_client import REGISTRY
+from prometheus_flask_exporter import PrometheusMetrics
 from werkzeug.middleware.proxy_fix import ProxyFix
-from flask_marshmallow import Marshmallow
-from config import DevelopmentConfig, TestingConfig
-from logger import setup_logger
-from app.routes import register_namespaces, register_routes
+
 from app.database import init_db, set_db_globals, setup_listeners
 from app.database.vacuum import start_background_task
-from app.utils.db_setting_tables import set_roles, set_object_status
 from app.error_handlers import setup_error_handlers
-
+from app.routes import register_namespaces, register_routes
+from app.utils.db_setting_tables import set_object_status, set_roles
+from config import DevelopmentConfig, TestingConfig
+from logger import setup_logger
 
 authorizations = {
-    'Bearer': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization',
-        'description': 'Добавьте JWT-токен в формате: Bearer <jwt_token>'
+    "Bearer": {
+        "type": "apiKey",
+        "in": "header",
+        "name": "Authorization",
+        "description": "Добавьте JWT-токен в формате: Bearer <jwt_token>",
     }
 }
 
@@ -45,13 +44,11 @@ def create_app(config_name="development"):
         metrics = PrometheusMetrics(app)
         # необязательно, но можно указать кастомные метрики
         if "app_info" not in REGISTRY._names_to_collectors:
-            metrics.info('app_info', 'Описание приложения', version='1.0.3')
+            metrics.info("app_info", "Описание приложения", version="1.0.3")
         app.config.from_object(DevelopmentConfig)
         # Настройка
         trace.set_tracer_provider(
-            TracerProvider(
-                resource=Resource.create({"service.name": "ok_service"})
-            )
+            TracerProvider(resource=Resource.create({"service.name": "ok_service"}))
         )
 
         jaeger_exporter = JaegerExporter(
@@ -75,15 +72,13 @@ def create_app(config_name="development"):
         x_for=1,  # Используем 1 прокси для заголовка X-Forwarded-For
         x_proto=1,  # Учитываем X-Forwarded-Proto (HTTP/HTTPS)
         x_host=1,  # Учитываем X-Forwarded-Host
-        x_port=1   # Учитываем X-Forwarded-Port
+        x_port=1,  # Учитываем X-Forwarded-Port
     )
     # Инициализация базы данных
-    engine, Session, Base = init_db(
-        app.config['SQLALCHEMY_DATABASE_URI'], config_name)
+    engine, Session, Base = init_db(app.config["SQLALCHEMY_DATABASE_URI"], config_name)
     set_db_globals(engine, Session, Base)
     logger = setup_logger()
-    logger.info("База данных успешно инициализирована.",
-                extra={'login': 'init'})
+    logger.info("База данных успешно инициализирована.", extra={"login": "init"})
 
     # Запуск фоновой задачи при старте приложения
 
@@ -101,33 +96,27 @@ def create_app(config_name="development"):
     # Инициализация JWT
     try:
         JWTManager(app)
-        logger.info(f"JWT инициализирован. {app.config['JWT_ACCESS_TOKEN_EXPIRES']}",
-                    extra={'login': 'init'})
+        logger.info(
+            f"JWT инициализирован. {app.config['JWT_ACCESS_TOKEN_EXPIRES']}",
+            extra={"login": "init"},
+        )
     except Exception as e:
-        logger.error(f"Ошибка при инициализации JWT: {e}",
-                     extra={'login': 'init'})
+        logger.error(f"Ошибка при инициализации JWT: {e}", extra={"login": "init"})
         raise
 
     # Регистрация маршрутов
     try:
         register_routes(app)
-        logger.info("Маршруты успешно зарегистрированы.",
-                    extra={'login': 'init'})
+        logger.info("Маршруты успешно зарегистрированы.", extra={"login": "init"})
     except Exception as e:
-        logger.error(f"Ошибка при регистрации маршрутов: {e}",
-                     extra={'login': 'init'})
+        logger.error(f"Ошибка при регистрации маршрутов: {e}", extra={"login": "init"})
         raise
 
     # Инициализация Marshmallow
     ma.init_app(app)
 
     # Инициализация API
-    api = Api(
-        app,
-        doc='/swagger',
-        security='Bearer',
-        authorizations=authorizations
-    )
+    api = Api(app, doc="/swagger", security="Bearer", authorizations=authorizations)
     # Регистрация маршрутов
     register_namespaces(api)
 
@@ -162,13 +151,13 @@ def create_app(config_name="development"):
 
             logger.info(
                 f"{method} {path} {status} {round(duration * 1000)}ms {user_agent}",
-                extra={"login": user_info}
+                extra={"login": user_info},
             )
         except Exception as e:
             logger.error(f"Error while logging request: {e}")
         return response
 
     # Настройка CORS
-    CORS(app, resources={r"/*": {"origins": '*'}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     return app
