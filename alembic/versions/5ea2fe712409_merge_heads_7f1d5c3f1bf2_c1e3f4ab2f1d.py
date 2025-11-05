2d00eca244bde90f2ba29adec1cbd244b6c0211b
viktoria <v.s.polikanova@gmail.com>
2025-11-05 23:36:39 +0700

    фикс мигарций

---
+++ alembic/versions/5ea2fe712409_merge_heads_7f1d5c3f1bf2_c1e3f4ab2f1d.py
@@ -0,0 +1,26 @@
+"""merge heads: 7f1d5c3f1bf2 + c1e3f4ab2f1d
+
+Revision ID: 5ea2fe712409
+Revises: 7f1d5c3f1bf2, c1e3f4ab2f1d
+Create Date: 2025-11-05 23:36:16.146221
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '5ea2fe712409'
+down_revision: Union[str, None] = ('7f1d5c3f1bf2', 'c1e3f4ab2f1d')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
