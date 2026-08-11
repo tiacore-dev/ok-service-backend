8a999ae0ae8c710da7d0444c46516568ce744787
viktoria <v.s.polikanova@gmail.com>
2026-08-11 15:54:34 +0700

    fix migration

--- alembic/versions/20260811_place_relations_permissions.py
+++ alembic/versions/20260811_place_relations_permissions.py
@@ -7,7 +7,7 @@ import sqlalchemy as sa
 
 from alembic import op
 
-revision: str = "20260811_place_relations_permissions"
+revision: str = "20260811_place_rel_perms"
 down_revision: Union[str, None] = "20260811_place_relations"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
